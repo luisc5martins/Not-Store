dbb488dbdcdd0680d628a2c1d2639e3da0520e54
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-07-09 16:57:52 -0300

    ajustes finos layout, chamada da API e criação de componente de logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s9b6kl6h9n"
+    "revision": "0.odc3sk8d4q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
