473e6c26dfe5a7e9079584a1ab939253ee3cdea8
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-07-10 16:54:02 -0300

    Configuração do Passage (1/2)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="6RTG1jMkkpWQfjRLzjQcI2rn"></passage-auth>
+  </div>
 </template>
+
+<style></style>
