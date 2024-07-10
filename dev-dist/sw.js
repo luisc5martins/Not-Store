431c5a690eed11454a4c40d55c87bc3900cf578c
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-07-10 16:07:30 -0300

    Criação de telas de Login

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.odc3sk8d4q"
+    "revision": "0.iltbki0m8ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/templates/small/SmallFooter.vue
+++ src/components/templates/small/SmallFooter.vue
@@ -1,5 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { onBeforeRouteUpdate } from 'vue-router';
+
+const showMenu = ref(false);
+
+onBeforeRouteUpdate(() => {
+  showMenu.value = false;
+});
+</script>
 <template>
+  <div id="footerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
+    <RouterLink to="/">
+      <i class="mdi mdi-home-outline" />
+      Home
+    </RouterLink>
+    <RouterLink to="/login">
+      <i class="mdi mdi-account" />
+      Login
+    </RouterLink>
+  </div>
   <div class="icons">
     <RouterLink to="/">
       <i class="mdi mdi-home-outline" />
@@ -13,51 +32,57 @@
       <i class="mdi mdi-cart-outline" />
       Carrinho
     </RouterLink>
-    <RouterLink to="/">
+
+    <div class="hamburger" @click="showMenu = !showMenu">
       <i class="mdi mdi-menu" />
       Menu
-    </RouterLink>
+    </div>
   </div>
 </template>
 <style scoped>
 #footerMenu {
-  position: fixed;
-  bottom: 15%;
-  right: 0;
+    position: fixed;
+    bottom: 15%;
+    right: 0;
 
-  width: 20%;
-  border-top: #eeeeee 1px solid;
-  background-color: white;
+    width: 20%;
+    border-top: #EEEEEE 1px solid;
+    background-color: white;
 
-  display: block;
-  padding: 1rem;
+    display: block;
+    padding: 1rem;
 }
 
 #footerMenu a {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  text-decoration: none;
-  color: #282828;
-  font-size: 1rem;
-  transition: color 0.3s;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    text-decoration: none;
+    color: #282828;
+    font-size: 1rem;
+    transition: color 0.3s;
 }
+
 
 .icons {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 1.7rem;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
 }
 
-.icons a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-decoration: none;
-  color: #282828;
-  font-size: 1rem;
-  transition: color 0.3s;
+.icons a,
+.icons .hamburger {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
+    color: #282828;
+    font-size: 1rem;
+    transition: color 0.3s;
+}
+
+.hamburger:hover {
+    cursor: pointer;
 }
 </style>
