fd68e0dfe8e510290346a6a4c69554ab62c674d1
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-07-25 15:04:44 -0300

    alterações de css e correções de arquivos js de categoria

--- src/components/templates/small/SmallFooter.vue
+++ src/components/templates/small/SmallFooter.vue
@@ -18,6 +18,18 @@ onBeforeRouteUpdate(() => {
       <i class="mdi mdi-account" />
       Login
     </RouterLink>
+    <router-link to="/categorias">
+      <i class="icon mdi mdi-car-multiple" /> Categorias
+    </router-link>
+    <router-link to="/acessorios">
+      <i class="icon mdi mdi-list-box" /> Acessórios
+    </router-link>
+    <router-link to="/cores">
+      <i class="icon mdi mdi-palette" /> Cores
+    </router-link>
+    <router-link to="/marcas">
+      <i class="icon mdi mdi-car-info" /> Marcas
+    </router-link>
   </div>
   <div class="icons">
     <RouterLink to="/">
@@ -41,48 +53,46 @@ onBeforeRouteUpdate(() => {
 </template>
 <style scoped>
 #footerMenu {
-    position: fixed;
-    bottom: 15%;
-    right: 0;
-
-    width: 20%;
-    border-top: #EEEEEE 1px solid;
-    background-color: white;
-
-    display: block;
-    padding: 1rem;
+  position: fixed;
+  bottom: 15%;
+  right: 0;
+  width: 20%;
+  border-top: #EEEEEE 1px solid;
+  background-color: white;
+  display: block;
+  padding: 1rem;
 }
 
 #footerMenu a {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    text-decoration: none;
-    color: #282828;
-    font-size: 1rem;
-    transition: color 0.3s;
+  display: flex;
+  width: 100%;
+  text-decoration: none;
+  color: #282828;
+  font-size: 1rem;
+  transition: color 0.3s;
 }
 
 
 .icons {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  width: 100%;
+  align-items: left;
+  justify-content: space-between;
 }
 
 .icons a,
 .icons .hamburger {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-decoration: none;
-    color: #282828;
-    font-size: 1rem;
-    transition: color 0.3s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: #282828;
+  font-size: 20px;
+  transition: color 0.3s;
 }
 
 .hamburger:hover {
-    cursor: pointer;
+  cursor: pointer;
 }
+
 </style>
--- src/stores/category.js
+++ src/stores/category.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-import CategoryService from '@/api/categorias';
+import CategoryService from '@/services/category';
 const categoryService = new CategoryService();
 
 export const useCategoryStore = defineStore('category', () => {
